03bec93b197574d07ebb1890342b55caf25a547e
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-01 21:22:56 +0000

    Removed hard coded objects from the state and enabled data to be stored and retrieved from local storage

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,23 +5,7 @@ import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
   
   handleChange = (id) => {
@@ -59,6 +43,37 @@ class TodoContainer extends React.Component {
     });
   };
 
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            title: updatedTitle,
+          }
+        }
+        return todo;
+      })
+    })
+  };
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+
+  componentDidUpdate(prevState, prevProps) { 
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp);
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -68,7 +83,8 @@ class TodoContainer extends React.Component {
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
+            deleteTodoProps={this.delTodo}
+            setUpdate ={this.setUpdate}
         />
         </div>
       </div>
