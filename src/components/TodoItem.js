1559624828b060af9d04f6062672928ad9b5d210
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-01 21:24:17 +0000

    Added state to enable editing of todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import styles from "./TodoItem.module.css"
 
 export default class TodoItem extends Component {
+
+  state = {
+    editing: false,
+  }
+
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   render() {
 
     const completedStyle = {
@@ -13,8 +34,18 @@ export default class TodoItem extends Component {
 
     const { completed, id, title } = this.props.todo
 
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return (
       <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={ viewMode }>
         <input
           type="checkbox"
           className={styles.checkbox}
@@ -24,6 +55,17 @@ export default class TodoItem extends Component {
          <span style={completed ? completedStyle : null}>  
           {title}
           </span>
+        </div>
+        <input
+          type="text"
+          className={styles.textInput}
+          style={editMode}
+          value={title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
       </li>
     )
   }
