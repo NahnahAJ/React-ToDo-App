fca6fedc5c756db52fa2c2253499a666d29ef885
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-01 08:30:09 +0000

    Passed props from parent to the child to enable the deletion of a task

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -6,9 +6,14 @@ export default class TodosList extends Component {
     return (
       <ul>
       {this.props.todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={this.props.handleChangeProps}
+          deleteTodoProps={this.props.deleteTodoProps}
+        />
       ))}
-    </ul>
+      </ul>
     )
   }
 }
