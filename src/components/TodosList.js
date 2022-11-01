bae6a623aab46e5c779562e0c108084c5c94ce4a
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-01 01:33:02 +0000

    Map through the props after receiving from the parent component and rendering on the page

---
+++ src/components/TodosList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoItem from "./TodoItem";
+
+export default class TodosList extends Component {
+  render() {
+    return (
+      <ul>
+      {this.props.todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+    )
+  }
+}
