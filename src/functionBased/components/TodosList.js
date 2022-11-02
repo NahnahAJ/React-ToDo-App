1d89b8b82dcd5980afc1878544a7e1a1c916cdc2
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-02 15:09:59 +0000

    Refactored code to use function components and hooks instead of class components

---
+++ src/functionBased/components/TodosList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const TodosList = (props) => {
+  const {
+    handleChangeProps, deleteTodoProps, setUpdate, todos,
+  } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
+
+TodosList.propTypes = {
+  todos: PropTypes.arrayOf(Object).isRequired,
+  handleChangeProps: PropTypes.func.isRequired,
+  deleteTodoProps: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
+};
+export default TodosList;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+// component file
+import TodoContainer from './functionBased/components/TodoContainer';
 
-//component file
-import TodoContainer from "./components/TodoContainer"
-
-//stylesheet
-import "./App.css"
+// stylesheet
+import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-  <TodoContainer />
-  </React.StrictMode>, document.getElementById("root")
-)
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
+  </React.StrictMode>, document.getElementById('root'),
+);
